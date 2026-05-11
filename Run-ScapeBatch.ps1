bb60ee45532232127c338c934cd436661d3375d3
rebellious0508-blip <rebellious0508@gmail.com>
2026-05-11 20:42:02 +0100

    Add files via upload

---
+++ Run-ScapeBatch.ps1
@@ -0,0 +1,10 @@
+{
+  "inputRoot": "D:\\SCAPE\\SCAPE-master\\后续实验组输入数据\\沉积物运输\\input",
+  "smokeOutputRoot": "D:\\SCAPE\\SCAPE-master\\后续实验组输入数据\\沉积物运输\\output\\_SMOKE_10yr",
+  "formalOutputRoot": "D:\\SCAPE\\SCAPE-master\\后续实验组输入数据\\沉积物运输\\output",
+  "analysisRoot": "D:\\SCAPE\\SCAPE-master\\后续实验组输入数据\\沉积物运输\\analyse\\quick_charts",
+  "smokeParallel": 12,
+  "formalParallel": 10,
+  "baseline": "ST00_BASE_CONTROL"
+}
+
